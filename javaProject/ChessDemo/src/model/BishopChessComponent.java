d1f5b34c6a77ec4a286a5ce371bea11ada53d1fa
Bei-jifeng <105062760+Bei-jifeng@users.noreply.github.com>
2022-05-16 18:00:20 +0800

    Update BishopChessComponent.java
    5.16 edition

--- javaProject/ChessDemo/src/model/BishopChessComponent.java
+++ javaProject/ChessDemo/src/model/BishopChessComponent.java
@@ -8,7 +8,7 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-//兵
+//象
 public class BishopChessComponent extends ChessComponent{
 
     //显示象的图片
@@ -33,7 +33,7 @@ public class BishopChessComponent extends ChessComponent{
 
 
     /**
-     * 在构造棋子对象的时候，调用此方法以根据颜色确定rookImage的图片是哪一种
+     * 在构造棋子对象的时候，调用此方法以根据颜色确定bishopImage的图片是哪一种
      *
      * @param color 棋子颜色
      */
@@ -61,50 +61,52 @@ public class BishopChessComponent extends ChessComponent{
      *
      * @param chessComponents 棋盘
      * @param destination     目标位置，如(0, 0), (0, 7)等等
-     * @return 车棋子移动的合法性
+     * @return 象棋子移动的合法性
      */
 
-//    @Override
-//    public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
-//        ChessboardPoint source = getChessboardPoint();
-//        if (source.getX() == destination.getX()) {
-//            int row = source.getX();
-//            for (int col = Math.min(source.getY(), destination.getY()) + 1;
-//                 col < Math.max(source.getY(), destination.getY()); col++) {
-//                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-//                    return false;
-//                }
-//            }
-//        } else if (source.getY() == destination.getY()) {
-//            int col = source.getY();
-//            for (int row = Math.min(source.getX(), destination.getX()) + 1;
-//                 row < Math.max(source.getX(), destination.getX()); row++) {
-//                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-//                    return false;
-//                }
-//            }
-//        } else { // Not on the same row or the same column.
-//            return false;
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
+        ChessboardPoint source = getChessboardPoint();
+        if ((source.getX() - destination.getX()) == (source.getY() - destination.getY())) {
+            int row = Math.min(source.getX(), destination.getX()) + 1;
+            int col = Math.min(source.getY(), destination.getY()) + 1;
+            while (col < Math.max(source.getY(), destination.getY())){
+                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
+                    return false;
+                }
+                row ++;
+                col ++;
+            }
+        } else if ((source.getX() - destination.getX() ) == -(source.getY() - destination.getY())){
+            int row = Math.min(source.getX(), destination.getX()) - 1;
+            int col = source.getX() < destination.getX()? source.getY() + 1:destination.getY() + 1;
+            while (col < Math.max(source.getY(), destination.getY())){
+                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
+                    return false;
+                }
+                row ++;
+                col --;
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
 
     /**
      * 注意这个方法，每当窗体受到了形状的变化，或者是通知要进行绘图的时候，就会调用这个方法进行画图。
      *
      * @param g 可以类比于画笔
      */
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-////        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
-//        g.drawImage(rookImage, 0, 0, getWidth() , getHeight(), this);
-//        g.setColor(Color.BLACK);
-//        if (isSelected()) { // Highlights the model if selected.
-//            g.setColor(Color.RED);
-//            g.drawOval(0, 0, getWidth() , getHeight());
-//        }
-//    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+//        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
+        g.drawImage(bishopImage, 0, 0, getWidth() , getHeight(), this);
+        g.setColor(Color.BLACK);
+        if (isSelected()) { // Highlights the model if selected.
+            g.setColor(Color.RED);
+            g.drawOval(0, 0, getWidth() , getHeight());
+        }
+    }
 }
-
-//修改成功了吗
