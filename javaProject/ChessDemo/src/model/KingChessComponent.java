0b3d020b3eb85fc3b0271e6a678c06430ac43467
Bei-jifeng <105062760+Bei-jifeng@users.noreply.github.com>
2022-05-16 18:01:58 +0800

    Update KingChessComponent.java
    5.16 edition

--- javaProject/ChessDemo/src/model/KingChessComponent.java
+++ javaProject/ChessDemo/src/model/KingChessComponent.java
@@ -33,7 +33,7 @@ public class KingChessComponent extends ChessComponent{
 
 
     /**
-     * 在构造棋子对象的时候，调用此方法以根据颜色确定rookImage的图片是哪一种
+     * 在构造棋子对象的时候，调用此方法以根据颜色确定kingImage的图片是哪一种
      *
      * @param color 棋子颜色
      */
@@ -64,45 +64,37 @@ public class KingChessComponent extends ChessComponent{
      * @return 车棋子移动的合法性
      */
 
-//    @Override
-//    public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
-//        ChessboardPoint source = getChessboardPoint();
-//        if (source.getX() == destination.getX()) {
-//            int row = source.getX();
-//            for (int col = Math.min(source.getY(), destination.getY()) + 1;
-//                 col < Math.max(source.getY(), destination.getY()); col++) {
-//                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-//                    return false;
-//                }
-//            }
-//        } else if (source.getY() == destination.getY()) {
-//            int col = source.getY();
-//            for (int row = Math.min(source.getX(), destination.getX()) + 1;
-//                 row < Math.max(source.getX(), destination.getX()); row++) {
-//                if (!(chessComponents[row][col] instanceof EmptySlotComponent)) {
-//                    return false;
-//                }
-//            }
-//        } else { // Not on the same row or the same column.
-//            return false;
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean canMoveTo(ChessComponent[][] chessComponents, ChessboardPoint destination) {
+        ChessboardPoint source = getChessboardPoint();
+        if (source.getX() == destination.getX() & Math.abs(source.getY() - destination.getY()) == 1) {
+            return true;
+        }
+        if (source.getY() == destination.getY() & Math.abs(source.getX() - destination.getX()) == 1) {
+            return true;
+        }
+        if (Math.abs(source.getX() - destination.getX()) == 1 & Math.abs(source.getY() - destination.getY()) == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * 注意这个方法，每当窗体受到了形状的变化，或者是通知要进行绘图的时候，就会调用这个方法进行画图。
      *
      * @param g 可以类比于画笔
      */
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-////        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
-//        g.drawImage(rookImage, 0, 0, getWidth() , getHeight(), this);
-//        g.setColor(Color.BLACK);
-//        if (isSelected()) { // Highlights the model if selected.
-//            g.setColor(Color.RED);
-//            g.drawOval(0, 0, getWidth() , getHeight());
-//        }
-//    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+//        g.drawImage(rookImage, 0, 0, getWidth() - 13, getHeight() - 20, this);
+        g.drawImage(kingImage, 0, 0, getWidth() , getHeight(), this);
+        g.setColor(Color.BLACK);
+        if (isSelected()) { // Highlights the model if selected.
+            g.setColor(Color.RED);
+            g.drawOval(0, 0, getWidth() , getHeight());
+        }
+    }
 }
