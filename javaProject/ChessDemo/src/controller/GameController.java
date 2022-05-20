3618bc87fa03d0ded8fd1d1519d56824e63e6648
Chenzq2333 <105898432+Chenzq2333@users.noreply.github.com>
2022-05-20 11:52:34 +0800

    Update GameController.java

--- javaProject/ChessDemo/src/controller/GameController.java
+++ javaProject/ChessDemo/src/controller/GameController.java
@@ -1,28 +1,42 @@
 package controller;
 
+import model.ChessComponent;
 import view.Chessboard;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameController {
     private Chessboard chessboard;
+
+    public static Integer step=0;
+    public static Map<String,ChessComponent[][]> chessBoardStatus=new HashMap<>();
+
 
     public GameController(Chessboard chessboard) {
         this.chessboard = chessboard;
     }
 
     public List<String> loadGameFromFile(String path) {
-        try {
-            List<String> chessData = Files.readAllLines(Path.of(path));
-            chessboard.loadGame(chessData);
-            return chessData;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            List<String> chessData = Files.readAllLines(Path.of(path));
+//            chessboard.loadGame(chessData);
+//            return chessData;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return null;
     }
 
+    public void restart(){
+        chessboard.restart();
+    }
+
+    public void repentance() {
+        chessboard.repentance();
+    }
 }
